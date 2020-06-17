b74effb8f4904b2ebcf36103e15928611c0dd1c5
Matt Ragone <m.ragone85@gmail.com>
2020-06-17 08:46:31 -0400

    Day2

---
+++ stringReversal/reversed.js
@@ -0,0 +1,15 @@
+// Given a string input, reverse the letters and return the reversed string.
+
+(function revString(str) {
+  let stringArr = str.split("");
+  let reversedStringArr = stringArr.reverse();
+  let reversedString = reversedStringArr.join("");
+
+  console.log(reversedString);
+})("Matthew");
+
+// function revString(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// console.log(revString("Brad Traversy"));
