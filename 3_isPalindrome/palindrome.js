11351ea71c0ec024f18a1329470bfa1f34d8ed58
Matt Ragone <m.ragone85@gmail.com>
2020-06-18 18:10:11 -0400

    Day 3

--- 3_isPalindrome/palindrome.js
+++ 3_isPalindrome/palindrome.js
@@ -1,18 +1,33 @@
 /* Write a function that takes in a string and determines 
     if that string is a palindrome */
 
-function isPalindrome(str) {
-  str = str.toLowerCase();
-  let charArr = str.split("");
-  let validChars = "abcdefghijklmnopqrstuvwxyz".split("");
+// EXAMPLE 1
+// function isPalindrome(str) {
+//   str = str.toLowerCase();
+//   let charArr = str.split("");
+//   let validChars = "abcdefghijklmnopqrstuvwxyz".split("");
 
-  let lettersArr = [];
-  charArr.forEach((char) => {
-    if (validChars.indexOf(char) > -1) lettersArr.push(char);
-  });
+//   let lettersArr = [];
+//   charArr.forEach((char) => {
+//     if (validChars.indexOf(char) > -1) lettersArr.push(char);
+//   });
 
-  if (lettersArr.join("") === lettersArr.reverse().join("")) return true;
-  else return false;
+//   if (lettersArr.join("") === lettersArr.reverse().join("")) return true;
+//   else return false;
+// }
+
+// console.log(isPalindrome("Madam, I'm Adam"));
+
+// EXAMPLE 2
+function palindrome(str) {
+  let reg = /[\W_]/g;
+
+  let smallStr = str.toLowerCase().replace(reg, "");
+
+  let reversed = smallStr.split("").reverse().join("");
+  if (reversed === smallStr) return true;
+
+  return false;
 }
 
-console.log(isPalindrome("Madam, I'm Adam"));
+console.log(palindrome("Poor Dan in a Droop"));
